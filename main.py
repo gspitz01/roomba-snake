3fb565c7a737ea414f8cccc73a560ab040446f84
Alvin-QuincyCollege <alvin.xie@qc.quincycollege.edu>
2017-05-05 15:49:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Roomba import *
 from random import shuffle  # shuffle was for if we wanted a randomized order for colors.
+from led import *
 import serial               # PySerial: https://pypi.python.org/pypi/pyserial
 import time                 # for sleep()
 import sys                  # for exit()
@@ -51,31 +52,6 @@ mainRoomba=MainRoomba("gold",roombaColors[currentRound])
 roombaCreate(mainRoomba)
 
 #Creates the main roomba and tells it that the next color it has to find.
-
-#All LED stuff, no need to touch.
-#Ascii values for LED stuff
-first=49
-second=50
-third=51
-space=32
-D=68
-O=79
-N=78
-E=69
-W=87
-F=70
-displayLED=lambda x:ser.write(bytearray([164,w,x,y,z]))
-buffer=lambda:cliff(space,space,space,space) #Clears the LED display
-
-#LED displays, ON and Off are sent to other roombas.
-display={
-1: lambda:displayLED(first,space,space,space),
-2: lambda:displayLED(second,space,space,space),
-3: lambda:displayLED(third,space,space,space),
-4: lambda:displayLED(D,O,N,E),
-"ON":lambda:displayLED(O,N,space,space),
-"OFF":lambda:displayLED(O,F,F,space)
-}
 
 #Turns on the main roomba
 ser.write(bytearray([128, 131]))
