f393aad571555e176f4f1d3a0dc1ed5045bb5eaa
Alvin-QuincyCollege <alvin.xie@qc.quincycollege.edu>
2017-05-05 16:06:06 -0400

    Update ledsanddrive.py
    Added in movement lambdas

--- ledsanddrive.py
+++ ledsanddrive.py
@@ -14,8 +14,15 @@ N=78
 E=69
 W=87
 F=70
-displayLED=lambda x:ser.write(bytearray([164,w,x,y,z]))
+velocity=100 #Speed at which the Roomba drives.
+
+displayLED=lambda w,x,y,z:ser.write(bytearray([164,w,x,y,z]))
+move=lambda y,z:ser.write(bytearray([137,0,x,y,z]))
 buffer=lambda:cliff(space,space,space,space) #Clears the LED display
+
+
+#Lambda example on how to use them
+#display[1]() will call displayLED which in turn calls ser.write(bytearray([w,x,y,z]));
 
 #LED displays, ON and Off are sent to other roombas.
 display={
@@ -26,3 +33,12 @@ display={
 "ON":lambda:displayLED(O,N,space,space),
 "OFF":lambda:displayLED(O,F,F,space)
 }
+
+movement={
+1: lambda: move(velocity,128,0)#Drive straight  
+2: lambda: move(velocity,0,1)  #Turn counterclockwise
+3: lambda: move(velocity,255)  #Turn clockwise
+4: lambda: move(0,0,0) #stop roomba
+}
+
+
