719d9b3608dc5ad9a87c82d56572a8cd7ff0edf7
Alvin-QuincyCollege <alvin.xie@qc.quincycollege.edu>
2017-05-10 01:19:45 -0400

    Update opcodes.py
    This may work or not. 
    Didn't test after converting this to a module.

--- opcodes.py
+++ opcodes.py
@@ -1,9 +1,10 @@
 import serial  # PySerial: https://pypi.python.org/pypi/pyserial
 
 ser = serial.Serial()
+
+
 # All LED stuff, no need to touch.
 # Ascii values for LED stuff
-
 FIRST = 49
 SECOND = 50
 THIRD = 51
@@ -14,26 +15,30 @@ N = 78
 E = 69
 W = 87
 F = 70
-VELOCITY = 100
+
+VELOCITY = 100 #Change velocity if you want the default velocity to change.
 
 
-def movement(y, z): return ser.write(bytearray([137, 0, VELOCITY, y, z]))
+movement= lambda x, y :ser.write(bytearray([137, 0, VELOCITY, x, y]))
+"""Makes the Roomba move in the direction wanted by changing the x and y values
 
 
-def display_led(w, x, y, z): return ser.write(bytearray([164, w, x, y, z]))
-
-
+display_led= lambda w, x, y, z : ser.write(bytearray([164, w, x, y, z]))
 """Changes the LED displays to whatever is in the w,x,y,z"""
 
 
-def buffer(): return display_led(SPACE, SPACE, SPACE, SPACE)
 
 
+buffer= lambda: display_led(SPACE, SPACE, SPACE, SPACE)
 """Clears the LED display"""
 
+********************************************************
 # Lambda example on how to use them
 # display[1]() will call display_led which in turn calls
-# ser.write(bytearray([w,x,y,z]));
+# display_led(FIRST, SPACE, SPACE, SPACE)
+# which in turn calls 
+# ser.write(164,49,32,32,32)
+********************************************************
 
 # LED displays, ON and Off are sent to other roombas.
 display = {
