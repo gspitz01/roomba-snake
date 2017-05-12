b4a2eb07f7fd9c831bb825893c01560c8f615962
Greg Spitz <gregspitz6@gmail.com>
2017-05-11 23:31:53 -0400

    Add mock Serial class to game_utilities.py

--- game_utilities.py
+++ game_utilities.py
@@ -1,4 +1,22 @@
 def game_output(*output_string):
     print(*output_string)
 
+class Srial:
+    def __init__(self):
+        pass
+
+    def open(self):
+        self.portstr = "This is a fake Serial"
+
+    def isOpen(self):
+        return True
+
+    def write(self, *what_to_write):
+        game_output("Writing to Roomba: ", what_to_write)
+
+    def close(self):
+        pass
+
+    def read(self):
+        return 
     
